35cca83410169c82c01aaef11b17696b8e46e3e9
Tarun_PC <Tarun_PC@192.168.100.8>
2020-04-30 09:33:59 +0530

    Behaviour data driven Framework is commited

---
+++ Nov_BDD/src/main/java/LIB/parameters.java
@@ -0,0 +1,38 @@
+package LIB;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+public class parameters
+{
+
+		WebDriver driver = null; 
+
+/*		@Given("^I go to (.*)$")
+		public void I_go_to_gmail(String url) throws Exception
+		{
+			   System.setProperty("webdriver.chrome.driver", "F:\\D-Drive\\FromC\\chromedriver_win321\\chromedriver.exe");
+			   driver=new ChromeDriver();
+			   driver.manage().window().maximize();
+			   driver.get("http://apps.qaplanet.in/qahrm");
+			   Thread.sleep(2000);
+		}*/
+
+	/*	@When("^I enter my username as (.*)$")
+		public void I_enter_my_username(String uname)
+		{
+			driver.findElement(By.name("txtUserName")).sendKeys(uname);
+		}
+
+		@And("^I enter my password as (.*)$")
+		public void I_enter_my_password(String pwd){
+		driver.findElement(By.name("txtPassword")).sendKeys(pwd);
+		}
+*/
+
+}
---
+++ Nov_BDD/src/test/java/FeatureFiles/Login.feature
@@ -0,0 +1,14 @@
+Feature: Verify OrangeHRM
+
+
+Scenario: Title of your scenario
+Given OpenHRM WebPage 
+Then User verified home page
+	And verify user name
+	And verify password
+When I Enter user name
+	And Enter password
+	And Click on login
+Then Check OrangeHRM Page
+	And Click on logout 
+
